c3c38be25214262078ef193ef80e3511680e2a36
captain0612 <aditya.sampath.k@gmail.com>
2021-06-01 18:06:02 +0530

    post item

--- src/main/java/com/csdemo/coffeestore/contract/ItemsResponse.java
+++ src/main/java/com/csdemo/coffeestore/contract/ItemsResponse.java
@@ -4,6 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @Builder
@@ -14,4 +19,64 @@ public class ItemsResponse {
     private int quantity;
 
     private int price;
+
+    public static void main(String args[]){
+        List<List<LinkedHashMap<String, String>>> lobs = new ArrayList<>();
+        List<LinkedHashMap<String, String>> value = new ArrayList<>();
+
+        LinkedHashMap<String, String> b1 = new LinkedHashMap<>();
+        LinkedHashMap<String, String> b2 = new LinkedHashMap<>();
+        LinkedHashMap<String, String> b3 = new LinkedHashMap<>();
+
+        b1.put("name", "CCB");
+        b1.put("id", "123");
+        b1.put("rsid", "12");
+
+        b2.put("name", "CAT>CLM>RAR");
+        b2.put("id", "1234");
+        b2.put("rsid", "12");
+
+        b3.put("name", "CHR");
+        b3.put("id", "5433");
+        b3.put("rsid", "43");
+
+        value.add(b1);
+        value.add(b2);
+        value.add(b3);
+
+        lobs.add(value);
+
+        List<String> lob1 = new ArrayList<>();
+        List<String> lob2 = new ArrayList<>();
+        List<String> lob3 = new ArrayList<>();
+
+        value.forEach(lob->{
+            List<String> lobNames = Arrays.asList(lob.get("name").split(">"));
+            if(lobNames.size()>0){
+                lob1.add(lobNames.get(0));
+            }
+            if(lobNames.size()>1){
+                lob2.add(lobNames.get(1));
+            }
+            if(lobNames.size()>2){
+                lob3.add(lobNames.get(2));
+            }
+        });
+        System.out.println("lob 1 :");
+        lob1.forEach(buss->{
+            System.out.print(buss);
+            System.out.println();
+        });
+        System.out.println("lob 2 :");
+        lob2.forEach(buss->{
+            System.out.print(buss);
+            System.out.println();
+        });
+        System.out.println("lob 3 :");
+        lob3.forEach(buss->{
+            System.out.print(buss);
+            System.out.println();
+        });
+
+    }
 }
--- src/main/java/com/csdemo/coffeestore/controller/ItemsController.java
+++ src/main/java/com/csdemo/coffeestore/controller/ItemsController.java
@@ -1,27 +1,31 @@
 package com.csdemo.coffeestore.controller;
 
 import com.csdemo.coffeestore.contract.ItemsResponse;
+import com.csdemo.coffeestore.dto.ItemsRequest;
+import com.csdemo.coffeestore.entity.Items;
+import com.csdemo.coffeestore.repository.ItemsRepository;
 import com.csdemo.coffeestore.service.ItemsService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/items")
-@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "http://localhost:4200")
 @Slf4j
 public class ItemsController {
 
     @Autowired
     private ItemsService itemsService;
+
+    @Autowired
+    private ItemsRepository itemRepo;
 
     @GetMapping
     public ResponseEntity getItemsList() {
@@ -34,5 +38,14 @@ public class ItemsController {
         }
 
         return new ResponseEntity<List<ItemsResponse>>(itemsMenu, HttpStatus.ACCEPTED);
+    }
+
+    @PostMapping
+    public ResponseEntity postItem(@RequestBody ItemsResponse itemRequest){
+        Items item = new Items();
+        BeanUtils.copyProperties(itemRequest,item);
+
+        itemRepo.save(item);
+        return new ResponseEntity<>( HttpStatus.ACCEPTED);
     }
 }
